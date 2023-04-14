44b3da90a483ab8944d857147e3d0b52ffc24dfb
Chris <xeon1989@gmail.com>
2023-04-14 22:25:34 +0800

    Update done.sh

--- Installation Script/done.sh
+++ Installation Script/done.sh
@@ -1,16 +1,17 @@
 #!/system/bin/sh
 export PATH=/sbin:/system/sbin:/system/xbin:/system/bin
-# mount /data partition in RW mode
-mount -o remount,rw /data /data
-# create a new folder in /data/app to place ES File Explorer apk file. Rename accordingly if you want to use other File Explorer.
-mkdir -p /data/app/ES/
-# copy apk from USB drive to the new folder . 
-cp /mnt/media_rw/udisk1/ES.apk /data/app/ES/ 
-# set appropriate permission
-chmod 644 /data/app/ES/ES.apk
-chmod 755 /data/app/ES/
-# delete the folder in USB drive to prevent re-run bash script after reboot
-rm -rf /mnt/media_rw/udisk1/b832bc61472727635baffcf25dd28e9f239273e2
+
+echo "====== show wireless charging to indicates script start ======="
+am start -n com.ecarx.gkuiconfig/.setting.SettingActivity 2>/dev/null
+input tap 700 200
+sleep 0.5
+am start -a android.settings.WPC_SETTINGS 2>/dev/null 
+#This open wireless charging, indicates start of script
+
+cd /mnt/media_rw/udisk1/
+pm install -rtdg --user 0 ES.apk
+
+mv /mnt/media_rw/udisk1/b832bc61472727635baffcf25dd28e9f239273e2 /mnt/media_rw/udisk1/b832bc61472727635baffcf25dd28e9f239273e2_done
 sync
 sleep 10
 reboot
