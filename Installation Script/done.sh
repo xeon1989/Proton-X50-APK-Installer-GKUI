5f1a16d9becbfbf1d3928e668a83a0adf8b484b4
Chris <xeon1989@gmail.com>
2023-04-16 01:58:17 +0800

    Update done.sh

--- Installation Script/done.sh
+++ Installation Script/done.sh
@@ -1,17 +1,30 @@
 #!/system/bin/sh
+logfilename="outputInstall_"
+timestamp="$(date +%s)"
+logfilename="$logfilename$timestamp"
+exec > /mnt/media_rw/udisk1/$logfilename.txt 2>&1
 export PATH=/sbin:/system/sbin:/system/xbin:/system/bin
+
+echo "Special thanks for enhancement and bugfixes by muncheno"
 
 echo "====== show wireless charging to indicates script start ======="
 am start -n com.ecarx.gkuiconfig/.setting.SettingActivity 2>/dev/null
-input tap 700 200
 sleep 0.5
+input tap 700 200
 am start -a android.settings.WPC_SETTINGS 2>/dev/null 
 #This open wireless charging, indicates start of script
 
-cd /mnt/media_rw/udisk1/
-pm install -rtdg --user 0 ES.apk
+pm install -rtdg --user 0 /mnt/media_rw/udisk1/ES.apk
 
+echo "====== renaming the script folder ========"
 mv /mnt/media_rw/udisk1/b832bc61472727635baffcf25dd28e9f239273e2 /mnt/media_rw/udisk1/b832bc61472727635baffcf25dd28e9f239273e2_done
+
+
+echo "=========== ending the script ============"
+am start -a android.settings.SETTINGS 	#show settings app to indicates script completed
+exec > /dev/null
+
+/system/bin/sh /system/bin/update.sh &
 sync
-sleep 10
-reboot
+#sleep 5
+#reboot
